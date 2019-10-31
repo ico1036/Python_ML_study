6af90963afd4b59436a39a0a6b5ce37bc620c3cc
ico1036 <jiwoong.kim@cern.ch>
2019-10-31 05:55:22 +0000

    a

--- N03_PYROOT/N03_SigBKG_classifier.py
+++ N03_PYROOT/N03_SigBKG_classifier.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import random
+import matplotlib
+import pandas as pd
+from IPython.display import display
+from tensorflow import keras
+from tensorflow.keras import layers
+
 
 # read data
 xy=np.loadtxt('data.csv', delimiter=',', dtype=np.float)
@@ -22,58 +29,59 @@ print(y_test.shape)
 # HyperParameter
 batch_size = 32
 training_epochs=10
-learning_rate = 0.001
+neu = 20
 
-# Place holder
-X = tf.placeholder(tf.float32,[None,3])
-Y = tf.placeholder(tf.float32,[None,1])
+## --Model
+x = layers.Input(shape=(3,))
 
-# Dropout
-keep_prob = tf.placeholder(tf.float32)
+h = layers.Dense(neu, activation='relu')(x)
+#h = layers.Dropout(0.5)(h)
 
-# Input Layer
-W1 = tf.get_variable("W1", shape=[3, 50],initializer=tf.contrib.layers.xavier_initializer()) 
-b1  = tf.Variable(tf.random_normal([50]),name='bias1')
-L1  =tf.nn.relu(tf.matmul(X,W1)+b1)
-L1 =tf.nn.dropout(L1, keep_prob=keep_prob)
+h = layers.Dense(neu, activation='relu')(x)
+#h = layers.Dropout(0.5)(h)
+h = layers.Dense(neu, activation='relu')(x)
 
-# Output Layer
-W2 = tf.get_variable("W2", shape=[50, 3],initializer=tf.contrib.layers.xavier_initializer()) 
-b2  = tf.Variable(tf.random_normal([3]),name='bias2')
-hypothesis  =tf.nn.sigmoid(tf.matmul(L1,W2)+b2)
+#h = layers.Dropout(0.5)(h)
 
+y = layers.Dense(1, activation='sigmoid')(h)
+model = tf.keras.Model(inputs = x,outputs = y)
+model.summary()
+model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
 
-# loss, optimizer
-loss = -tf.reduce_mean(Y*tf.log(hypothesis) + (1-Y) * tf.log(1-hypothesis))
-train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
-
-# Acc & Prediction
-predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
-
-# Initialize 
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+model_weights = 'model_weights_log.h5'
+predictions_file = 'prediction_nn_log.pyc'
 
 
-# Train
-print('Learning started')
+try:
+    model.load_weights(model_weights)
+    print('Weights loaded from ' + model_weights)
+except IOError:
+    print('No pre-trained weights found')
+try:
+    model.fit(x_train, y_train,
+        batch_size=batch_size,
+        epochs=training_epochs,
+        verbose=1,
+        callbacks = [
+            tf.keras.callbacks.EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+            tf.keras.callbacks.ModelCheckpoint(model_weights,
+            monitor='val_loss', verbose=True, save_best_only=True)
+        ],
+        validation_data=(x_test, y_test)
+    )
+except KeyboardInterrupt:
+    print('Training finished early')
 
-for epoch in range(training_epochs):
-    batch_count = int(x_train.shape[0]/batch_size)
-    avg_loss = 0
-    for i in range(batch_count):
-        batch_xs, batch_ys = x_train[i*batch_size : i*batch_size+batch_size],y_train[i*batch_size : i*batch_size+batch_size]
-        feed_dict = feed_dict={X: batch_xs, Y: batch_ys , keep_prob: 0.7}
-        c,acc,_ = sess.run([loss,accuracy,train],feed_dict=feed_dict) 
-        avg_loss += c / batch_count
-    print('Epoch:', '%04d' % (epoch+1), "Loss =, {:.9f}   Acc: {:.2%}".format(avg_loss,acc))
+model.load_weights(model_weights)
+yhat = model.predict(x_test, verbose=1, batch_size=batch_size)
+       # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+       # print 'Validation loss:', score[0]
+       # print 'Validation accuracy:', score[1]
+np.save(predictions_file, yhat)
 
-
-print('Learning Finished')
-
-
-#Test model and check accuracy
-print('Accuracy', sess.run(accuracy, feed_dict={X: x_test, Y: y_test, keep_prob:1}))
-
+test_loss, test_acc = model.evaluate(x_test,y_test)
+print('test_acc: ', test_acc)
 
