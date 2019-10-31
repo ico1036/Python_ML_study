dd672e618c3ad43d40b114eb041c4bbd17de655e
ico1036 <jiwoong.kim@cern.ch>
2019-10-31 08:29:22 +0000

    a

--- N03_PYROOT/N04_draw_result.py
+++ N03_PYROOT/N04_draw_result.py
@@ -4,14 +4,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-
-
-
 y_pred = np.load('prediction_nn_log.pyc.npy')
 test_data = np.loadtxt('test_data.csv',delimiter=',')
 y_true = test_data[:,-1]
-
-
 
 idx_sig = np.where(y_true == 1)[0]
 idx_bkg =np.where(y_true == 0)[0]
@@ -27,4 +22,3 @@ bins = np.linspace(0,1,100)
 plt.hist(hist_pred_sig,bins=bins,color='r',alpha=0.7)
 plt.hist(hist_pred_bkg,bins=bins,color='b',alpha=0.7)
 plt.savefig("score.png")
-#print(idx_sig)
